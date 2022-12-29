5662202df18b049a3fc06ff998c8a3bfb9ae9d84
polskiprogramistanet <rav@polskiprogramista.net>
2022-12-29 17:27:07 +0100

    Dodaj pliki projektów.

---
+++ GenericDelegates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesExample
+{
+    public class GenericDelegates
+    {
+        public void Action(Action action)
+        {
+            action();
+        }
+
+        public void ActionWithParameter(Action<string> action)
+        {
+            action(obj: "Action With Parameter executed");
+        }
+
+        public bool FuncThatOnlyReturns(Func<bool> func)
+        {
+            return func();
+        }
+        public bool FuncThatReturnAndNeedsParameter(Func<string, bool> func)
+        {
+            return func(arg: "Func That Returns And Needs Parameter executed");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DelegatesExample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var presentation = new GenericDelegates();
+
+            presentation.Action(() => Console.WriteLine("Action executed"));
+            presentation.ActionWithParameter((param) => Console.WriteLine(param));
+            presentation.FuncThatOnlyReturns(() =>
+            {
+                Console.WriteLine("Func That Only Return");
+                return true;
+            });
+            presentation.FuncThatReturnAndNeedsParameter((param) =>
+            {
+                Console.WriteLine(param);
+                return true;
+            });
+        }
+
+        
+    }
+}
